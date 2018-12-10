2cc2e83c5eb1d4a6ad7d849b4c05db7f20d99ff3
epomp447 <32994398+epomp447@users.noreply.github.com>
2018-12-09 22:04:14 -0500

    migrating toward header file

--- stack.c
+++ stack.c
@@ -20,6 +20,7 @@ void  freeStack(void);
 void  push(float);
 float pop(void);
 float top(void);
+void eval(char);
 
 /* ********************************** */
 /** \brief Main application entry point.
@@ -27,64 +28,48 @@ float top(void);
 * \param nothing
 * \returns nothing
 */
-main(){
+int main(){
    float op1, op2;
    char  expr[256];
    char* nextPtr;
 
   // allocate space for stack
    initStack();
-
-   printf("Enter postfix expression to be evaluated: ");
-   gets(expr);
-
-   nextPtr = strtok(expr," ");  
-   while (nextPtr != NULL) {
-      printf("Token: %s\n", nextPtr);
-      printf("atof: %f\n\n", atof(nextPtr));
-      if (atof(nextPtr) != 0) {
-         push(atof(nextPtr));
-      } 
-      else if (*nextPtr == '+') { // if token is addition
-         op1=pop();
-         op2=pop();
-         push(op1+op2);
-      }
-      else if (*nextPtr == '-') { // if token is subtraction
-         op1=pop();
-         op2=pop();
-         push(op2-op1);
-      }
-      else if (*nextPtr == '*') { // if token is multiplication
-         op1=pop();
-         op2=pop();
-         push(op1*op2);
-      }
-      else if (*nextPtr == '/') { // if token is division
-         op1=pop();
-         op2=pop();
-         push(op2/op1);
-      }
-      else if (*nextPtr == '=') { // if token is assignment
-         op1=pop();
-         op2=pop();
-         op1=op2;
-         push(op2);
-      }
-    /*TODO:multi-character character constants  ==, !=, &&, ||
-     *#define EQU_OP in tinylex? 
-     */
-      nextPtr = strtok(NULL," ");
-   }
-
-  // display the expression result
+   push(3);
+   push(4);
+   eval('+'); 
+ // display the expression result
    printf("Result: %f\n\n", top());
-
+    
   // deallocate space
    freeStack();
-
+   return 0;
 }
-   
+void eval(char op){
+   float op1, op2;
+   if (op == '+') { // if token is addition
+      op1=pop();
+      op2=pop();
+      push(op1+op2);
+   }
+   else if (op == '-') { // if token is subtraction
+      op1=pop();
+      op2=pop();
+      push(op2-op1);
+   }
+   else if (op == '*') { // if token is multiplication
+      op1=pop();
+      op2=pop();
+      push(op1*op2);
+   }
+   else if (op == '/') { // if token is division
+      op1=pop();
+      op2=pop();
+      push(op2/op1);
+   }
+   else 
+      printf("error on stack");
+}  
 /* ********************************** */
 
 /** \brief calloc memory for stack.
